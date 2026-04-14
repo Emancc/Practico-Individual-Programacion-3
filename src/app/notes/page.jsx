2afdcbaee5becbb12ed4b145424affa20146af58
Emancc <e.cepedacaceres@itecriocuarto.org.ar>
2026-04-13 22:49:20 -0300

    Navbar-personalizado

--- src/app/notes/page.jsx
+++ src/app/notes/page.jsx
@@ -4,8 +4,8 @@ import React from "react";
 
 export default function Notes() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-gray-900">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-gray-900 sm:items-start">
+    <div className="flex flex-col flex-1 items-center justify-center bg-capuchino font-sans">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             Notas
